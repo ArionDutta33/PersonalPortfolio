28fcf0d97307d57de2bbc90c5a7369a6051b4fa2
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-08-07 23:04:14 +0530

    uptil the index page

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
-const page2 = document.querySelector(".page2")
-gsap.to(page2, {
-    // transform: "translateY(-100%)"
+function mainAnimation() {
+    const page2 = document.querySelector(".page2")
+    gsap.to(page2, {
+        // transform: "translateY(-100%)"
 
-    y: "-100%",
-    scrollTrigger: {
-        markers: true,
-        scroller: "body",
-        trigger: ".page1",
-        pin: true,
-        scrub: 5,
-        start: " top -100%",
-        end: "bottom 40%",
-        // scrub: true,
+        y: "-100%",
+        scrollTrigger: {
+            // markers: true,
+            scroller: "body",
+            trigger: ".page1",
+            pin: true,
+            scrub: 5,
+            start: " top -100%",
+            end: "bottom 40%",
+            // scrub: true,
 
-    }
-})
+        }
+    })
+
+}
+mainAnimation()
 
 
-// gsap.registerPlugin(ScrollTrigger);
+const menuIcon = document.querySelector("i")
+console.log(menuIcon)
+// menuIcon.addEventListener("click", function () {
 
-// const page2 = document.querySelector(".page2");
+// })
 
-// gsap.to(page2, {
-//     y: "-100%",
-//     duration: 2,
-//     scrollTrigger: {
-//         markers: true,
-//         scroller: "body",
-//         trigger: ".page1",
-//         start: "top top",
-//         end: "bottom 90%",
-//         scrub: true // This ensures the animation is tied to the scrollbar
-//     }
-// });
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    // pagination: {
+    //     el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+});
