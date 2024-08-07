4ea53e5c0fceb173655afe65e72d764d23e6f59e
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-08-07 20:50:28 +0530

    gsap animation added

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ const page2 = document.querySelector(".page2")
 gsap.to(page2, {
     // transform: "translateY(-100%)"
     duration: 2,
-    y: "-100%"
+    y: "-100%",
+    // scrollTrigger: {
+    //     markers: true,
+    //     scroller: "body",
+    //     trigger: ".page1",
+    //     start: "top 20%"
+    // }
 })
