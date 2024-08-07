11173c336371b54fed78b1870828931040e21f20
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-08-07 21:31:32 +0530

    finally gsap done

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const page2 = document.querySelector(".page2")
 gsap.to(page2, {
     // transform: "translateY(-100%)"
-    duration: 2,
+
     y: "-100%",
-    // scrollTrigger: {
-    //     markers: true,
-    //     scroller: "body",
-    //     trigger: ".page1",
-    //     start: "top 20%"
-    // }
+    scrollTrigger: {
+        markers: true,
+        scroller: "body",
+        trigger: ".page1",
+        pin: true,
+        start: " top -100%",
+        end: "bottom -100%",
+        scrub: true,
+
+    }
 })
+
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// const page2 = document.querySelector(".page2");
+
+// gsap.to(page2, {
+//     y: "-100%",
+//     duration: 2,
+//     scrollTrigger: {
+//         markers: true,
+//         scroller: "body",
+//         trigger: ".page1",
+//         start: "top top",
+//         end: "bottom 90%",
+//         scrub: true // This ensures the animation is tied to the scrollbar
+//     }
+// });
